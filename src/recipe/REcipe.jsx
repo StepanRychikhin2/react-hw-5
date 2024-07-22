9396c47a786ae62e798bae1b9299eb5035301672
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-07-22 19:06:05 +0300

    awdwwdd

--- src/recipe/REcipe.jsx
+++ src/recipe/REcipe.jsx
@@ -6,11 +6,15 @@ const Recipe = ({ data, itherator }) => {
 
   let i = 0;
   return (
-    <li key={itherator} >
-      <p >Name: <span >{data.name}</span></p>
-      <p >Year: {data.year}</p>
-      <p >Ingdients:</p>
-      <img src={data.img} alt={data.name}/>
+    <li key={itherator}>
+        <img src={data.img} alt={data.name}/>
+      <p ><span >{data.time}</span>min</p>
+      <p ><span >{data.servining}</span>servicios</p>
+      <p ><span >{data.calories}</span>calories</p>
+      <h2>Difficulty</h2>
+      <p >Easy</p>
+      <p >Medium</p>
+      <p >Hard</p>
     </li>
   );
 };
